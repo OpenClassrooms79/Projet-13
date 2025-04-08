e69b8849ee05b586b6ddd96fb772154be092af51
Jocelyn <J0celyn@users.noreply.github.com>
2025-04-08 03:53:34 +0200

    compte utilisateur : amélioration d'affichage lorsqu'il n'a aucune commande

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -59,6 +59,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->orders = new ArrayCollection();
+        $this->roles = ['ROLE_USER'];
     }
 
     public function getId(): ?int
